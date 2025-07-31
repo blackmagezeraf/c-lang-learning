bce0e2ed94113b3e1777920a84b46313b9fedc5f
Ali Hussain Abid <31208110+blackmagezeraf@users.noreply.github.com>
2025-07-31 14:41:50 +0500

    Fixed Documentation Issues.

--- includes/BodyMassIndex.h
+++ includes/BodyMassIndex.h
@@ -1,26 +1,35 @@
-#pragma once
 /**
- * @headerfile BodyMassIndex.h
- * @brief Header file for Body Mass Index (BMI) calculations.
+ * @file BodyMassIndex.h
+ * @author Ali Hussain Abid
+ * @brief Calcualte Body Mass Index.
+ * @version 1.0
+ * @date 2025-07-31
+ *
+ * @copyright Copyright (c) 2025
+ *
  */
+
+#pragma once
 
 /**
  * @brief Function to Calculate Body Mass Index (BMI).
- * 
+ *
  * @param age The age of the person in years.
  * @param weight The weight of the person in kilograms.
  * @param height The height of the person in centimeters.
  */
-double CalculateBmiByAge(const int age, const float weight, const int height) {
-	/**
-	 * @note The height is divided by 100.0f to convert it from centimeters to meters.
-	 * This is necessary because BMI is calculated using height in meters.
-	 */
 
-	/**
-	 * @note The BMI formula is weight divided by the square of height in meters.
-	 * This is the standard formula used to calculate BMI.
-	 */
+/**
+ * @note The height is divided by 100.0f to convert it from centimeters to meters.
+ * This is necessary because BMI is calculated using height in meters.
+ */
+
+/**
+ * @note The BMI formula is weight divided by the square of height in meters.
+ * This is the standard formula used to calculate BMI.
+ */
+double CalculateBmiByAge(const int age, const float weight, const int height)
+{
 
 	double heightInMeters = height / 100.0f;
 
