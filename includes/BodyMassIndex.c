b9d51ee928205a0f4fc8746e39264c49ab2f1da1
Ali Hussain Abid <31208110+blackmagezeraf@users.noreply.github.com>
2025-08-03 12:03:57 +0500

    Remove Unnessary Comments

--- includes/BodyMassIndex.c
+++ includes/BodyMassIndex.c
@@ -2,9 +2,5 @@
 
 double CalculateBmiByAge(const int age, const float weight, const int height){
 	double heightInMeters = height / 100.0f;
-
-	/**
-	 * @return Returns the Calculated BMI
-	 */
 	return (double)(weight / (heightInMeters * heightInMeters));
 }
